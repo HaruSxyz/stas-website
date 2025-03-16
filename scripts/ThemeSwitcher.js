d105926cc522d089653b59be627ea2e703dfcb15
HaruSxyz <stas.aru.2006@gmail.com>
2025-03-16 21:50:31 +0100

    added theme changer, almost finished change animation

--- scripts/ThemeSwitcher.js
+++ scripts/ThemeSwitcher.js
@@ -12,6 +12,23 @@ class ThemeSwitcher {
     constructor() {
         this.orbit = document.querySelector(this.selectors.orbit);
         this.button = document.querySelector(this.selectors.button);
+
+        // Проверяем сохранённое состояние темы
+        const storedTheme = localStorage.getItem('themeIsLight') === 'true';
+        if (storedTheme) {
+            document.documentElement.classList.add('theme-is-light');
+            // Устанавливаем угол 90° без анимации:
+            this.state.angle = 90;
+            if (this.orbit) {
+                // Отключаем анимацию временно:
+                this.orbit.style.transition = 'none';
+                this.orbit.style.transform = `rotate(${this.state.angle}deg)`;
+                // Форсируем перерасчёт стилей:
+                void this.orbit.offsetWidth;
+                // Возвращаем стандартное значение transition (если оно задано в CSS)
+                this.orbit.style.transition = '';
+            }
+        }
         if (this.orbit && this.button) {
             this.bindEvents();
         }
@@ -19,17 +36,26 @@ class ThemeSwitcher {
 
     rotateOrbit = () => {
         if (this.state.isAnimating) return;
-
         this.state.isAnimating = true;
         this.button.disabled = true;
 
+        // Переключаем тему и сохраняем состояние
+        if (document.documentElement.classList.contains('theme-is-light')) {
+            document.documentElement.classList.remove('theme-is-light');
+            localStorage.setItem('themeIsLight', 'false');
+        } else {
+            document.documentElement.classList.add('theme-is-light');
+            localStorage.setItem('themeIsLight', 'true');
+        }
+
+        // Накопительное вращение: добавляем 90° к текущему углу
         this.state.angle += 90;
         this.orbit.style.transform = `rotate(${this.state.angle}deg)`;
 
         setTimeout(() => {
             this.state.isAnimating = false;
             this.button.disabled = false;
-        }, 2200); // 1000 = 1 sec (2 sec for animation)
+        }, 2200);
     };
 
     bindEvents() {
